0e3289f2272423e037ac5876aad23d1cce9c69f3
Ivan <zorromen@mail.ru>
2022-05-14 09:48:20 +0300

    Добавьте файлы проекта.

---
+++ GeneratorLCD/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace GeneratorLCD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.save = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDownH = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownW = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownH)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownW)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(349, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 87);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Расчет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(349, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 133);
+            this.textBox1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(469, 151);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(92, 40);
+            this.save.TabIndex = 6;
+            this.save.Text = "Сохранить";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(469, 198);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 40);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Открыть";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDownH
+            // 
+            this.numericUpDownH.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDownH.Location = new System.Drawing.Point(441, 244);
+            this.numericUpDownH.Maximum = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            this.numericUpDownH.Name = "numericUpDownH";
+            this.numericUpDownH.Size = new System.Drawing.Size(120, 32);
+            this.numericUpDownH.TabIndex = 8;
+            // 
+            // numericUpDownW
+            // 
+            this.numericUpDownW.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDownW.Location = new System.Drawing.Point(441, 284);
+            this.numericUpDownW.Maximum = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            this.numericUpDownW.Name = "numericUpDownW";
+            this.numericUpDownW.Size = new System.Drawing.Size(120, 32);
+            this.numericUpDownW.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(345, 248);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 24);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Высота";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(345, 288);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 24);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Ширина";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(567, 328);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDownW);
+            this.Controls.Add(this.numericUpDownH);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownH)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownW)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericUpDownH;
+        private System.Windows.Forms.NumericUpDown numericUpDownW;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ GeneratorLCD/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+
+namespace GeneratorLCD
+{
+
+  
+
+
+
+    public partial class Form1 : Form
+    {
+        short[] line = new short[16];
+
+        short H = 16;
+        short W = 16;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int y = 0; y < 16; y++)
+            {
+                for (int x = 15; x >= 0; x--)
+                {
+                    Button newbtn = new Button();
+                    newbtn.FlatAppearance.BorderSize = 1;
+                    newbtn.FlatStyle = FlatStyle.Flat;
+                    newbtn.Location = new Point((15 - x) * 20 + 22, 22 + y* 20);
+                    newbtn.Name = "newbtn";
+                    newbtn.Size = new Size(20, 20);
+                    String s;
+                    s = string.Format("{0:X1}", x)+string.Format("{0:X1}", y);
+                    newbtn.Tag = s;
+                    newbtn.UseVisualStyleBackColor = true;
+                    newbtn.BackColor = 0xFF1e2327.ToColor();
+                    Controls.Add(newbtn);
+                }
+
+                Label l = new Label();
+                l.Text = (y+1).ToString();
+                l.Size = new Size(24, 20);
+                l.Location = new Point(1, y * 20+22);
+                l.ForeColor = 0xFFFFFFFF.ToColor();
+                l.TextAlign = ContentAlignment.TopRight;
+                Controls.Add(l);
+
+                Label l1 = new Label();
+                l1.Text = (y + 1).ToString();
+                l1.Size = new Size(24, 20);
+                l1.Location = new Point(y * 20 + 20, 2);
+                l1.ForeColor = 0xFFFFFFFF.ToColor();
+                l1.TextAlign = ContentAlignment.BottomCenter;
+                Controls.Add(l1);
+
+            }
+
+            numericUpDownH.Value = 8;
+            numericUpDownW.Value = 16;
+
+            foreach (var c in Controls)
+            {
+                if (c is Button)
+                {
+                    ((Button)c).Click += (sender, e) =>
+                    {
+                        Button b = (Button)sender;
+                        if (b.Tag == null) return;       
+                        String s = b.Tag.ToString();
+                        short intX = short.Parse(s[0].ToString(), System.Globalization.NumberStyles.HexNumber);
+                        short intY = short.Parse(s[1].ToString(), System.Globalization.NumberStyles.HexNumber);
+                        line[intY] = (short)(line[intY] ^ ((short) (1 << intX)));// инверсия бита                                                            
+                        if ((line[intY] & ((short)(1 << intX))) != 0)
+                            b.BackColor = 0xFF23c1ff.ToColor();     
+                        else
+                            b.BackColor = 0xFF1e2327.ToColor();
+                    };
+                }
+            }
+
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String s = "";
+            s += string.Format("0x{0:X4}", (short)(numericUpDownH.Value)) + ", //H=" + numericUpDownH.Value.ToString() + Environment.NewLine;
+            s += string.Format("0x{0:X4}", (short)(numericUpDownW.Value)) + ", //W=" + numericUpDownW.Value.ToString() + Environment.NewLine;
+
+            for (int i = 0;  i < H; i++)
+            {
+                s += string.Format("0x{0:X4}", line[i]) + ", ";
+            }
+            textBox1.Text = s;
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            String s = textBox1.Text;
+            saveFileDialog1.Filter = "LCD Icon|*.icon";
+            saveFileDialog1.Title = "Save an LCD Icon File";
+            saveFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.
+            if (saveFileDialog1.FileName != "")
+            {
+                File.WriteAllText(saveFileDialog1.FileName, s);
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String s = "";
+            openFileDialog1.Filter = "LCD Icon|*.icon";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                    var sr = new StreamReader(openFileDialog1.FileName);
+                    s = sr.ReadToEnd();
+                    textBox1.Text = s;
+
+                    convertStrintToLine(s);
+                    lineToButton();
+            }
+        }
+
+        private void convertStrintToLine(String s)
+        {
+            String[] strline = new String[18];
+            Regex regex = new Regex(@"0x*(\w*)");
+            MatchCollection matches = regex.Matches(s);
+            if (matches.Count == 18)
+            {
+                for (int i = 0; i < 18; i++) {
+                    strline[i] = matches[i].Value;
+                    strline[i] = strline[i].Substring(2);
+                }
+                H = short.Parse(strline[0], System.Globalization.NumberStyles.HexNumber); 
+                W = short.Parse(strline[1], System.Globalization.NumberStyles.HexNumber);
+
+                numericUpDownH.Value = H;
+                numericUpDownW.Value = W;
+
+                for (int i = 0; i < 16; i++)
+                    line[i] = short.Parse(strline[2+i], System.Globalization.NumberStyles.HexNumber);
+            }
+        }
+
+        private void lineToButton()
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c is Button)
+                {
+                    if (c.Tag != null)
+                    {
+                        String str = c.Tag.ToString();
+                        short intX = short.Parse(str[0].ToString(), System.Globalization.NumberStyles.HexNumber);
+                        short intY = short.Parse(str[1].ToString(), System.Globalization.NumberStyles.HexNumber);
+
+                        if ((line[intY] & ((short)(1 << intX))) != 0)
+                            c.BackColor = 0xFF23c1ff.ToColor();
+                        else
+                            c.BackColor = 0xFF1e2327.ToColor();
+                    }
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+
+    static class ExtensionMethods
+    {
+        public static Color ToColor(this uint argb)
+        {
+            return Color.FromArgb((byte)((argb & -16777216) >> 0x18),
+                                  (byte)((argb & 0xff0000) >> 0x10),
+                                  (byte)((argb & 0xff00) >> 8),
+                                  (byte)(argb & 0xff));
+        }
+    }
+}
